d958295e9823ccb61a2859d0a381c7d157100fed
gr8alpaca <zackhugo2@gmail.com>
2024-12-11 17:27:02 -0500

    Day 11 (Part 2)

--- 11.cpp
+++ 11.cpp
@@ -6,10 +6,40 @@
 #include <math.h>
 #include <regex>
 #include <regex.h>
+#include <map>
 using namespace std;
 
-
+map<size_t, size_t> mp;
 vector<unsigned long long> v;
+
+vector<size_t> keys(map<size_t, size_t> &m){
+    vector<size_t> k;
+    for (map<size_t, size_t>::iterator i = m.begin(); i != m.end(); ++i)
+        k.push_back(i->first);
+    return k;
+}
+
+vector<size_t> values(map<size_t, size_t> &m){
+    vector<size_t> k;
+    for (map<size_t, size_t>::iterator i = m.begin(); i != m.end(); ++i)
+        k.push_back(i->second);
+    return k;
+}
+
+
+void add(size_t key, size_t v, map<size_t, size_t> &m){
+    (m.count(key) > 0) ? m[key] += v : m[key] = v;
+    // for (map<size_t, size_t>::iterator i = m.begin(); i != m.end(); ++i){
+    //     if ((i->first) == key){
+    //         m[key] += v;
+    //         return;
+    //         }
+    //     }
+    // m[key] = v;
+}
+
+
+
 
 int ctoi(char c){
     return int(c - '0');
@@ -21,40 +51,37 @@ void save_log(string path, string txt){
     of.close();
 }
 
-string str(){
+string str(){ 
     string s = "";
-    for (size_t i = 0; i < v.size(); i++)
-        s += to_string(v[i]) + " ";
+    for (map<size_t, size_t>::iterator i = mp.begin(); i != mp.end(); ++i)
+        s += to_string(i->first) + ": " + to_string(i->second) + " | ";
     return s;
 }
 
-void split(size_t i){
-    string s = to_string(v[i]);
-    int digits = s.length(); 
-    v[i] = stoull(s.substr(0, digits/2));
-    v.push_back(stoull(s.substr(digits/2, digits/2)));
+void split(size_t key, size_t qty, map<size_t, size_t> &m){
+    string s = to_string(key);
+    int digits = s.length();
+    add(stoull(s.substr(0, digits / 2)), qty, m);
+    add(stoull(s.substr(digits / 2, digits / 2)), qty, m);
 }
 
 
-void apply_blink(size_t i){
-    if (v[i] == 0){
-        v[i] = 1;
-        return;
-        }
-    
-    if (int(floor(log10(v[i])) + 1) % 2 == 0){
-        split(i);
-        return;
-        }
+void apply_blink(size_t key, size_t qty, map<size_t, size_t> &m){
+    if (key == 0)
+        return add(1, qty, m);
+        
+    if (int(floor(log10(key)) + 1) % 2 == 0)
+        return split(key, qty, m);
 
-    v[i] = v[i] * 2024;
-
+    add(key * 2024, qty, m);
 }
 
 void blink(){
-    size_t size = v.size();
-    for (size_t i = 0; i < size; i++)
-        apply_blink(i);
+    map<size_t, size_t> n;
+    for (map<size_t, size_t>::iterator i = mp.begin(); i != mp.end(); ++i){
+        apply_blink(i->first, i->second, n);
+    }
+    mp = n;
 }
 
 int main(){
@@ -67,19 +94,21 @@ int main(){
 
 
     string s;
-    while(getline(f, s, ' ')){
-        v.push_back(stoull(s));
-    }
+    while(getline(f, s, ' '))
+        add(stoull(s), 1, mp);
+    
     cout << str() << endl;
-
-    const size_t BLINK_AMOUNT = 75; 
-    for (size_t i = 0; i < BLINK_AMOUNT; i++){
+    const int LOOP_COUNT = 75;
+    for (size_t i = 0; i < LOOP_COUNT; i++){
+        cout << "\n - - - BLINK " << i << " - - - \n";
         blink();
-        cout << "\nBLINK #" << i << endl;//"\n\t"<< str() << endl;
     }
 
-
-    cout << "SCORE => " << v.size() << endl;
+    save_log("log.txt", str());
+    size_t sum = 0;
+    for (size_t i : values(mp))
+        sum += i;
+    cout << "FINAL SUM => " << sum << endl;
 
     return 0;
 }
